3de7ee1864a43b5e45dfe2241f5bd16578d65b19
lakshay007 <dmslakshay@gmail.com>
2025-06-05 13:09:18 +0530

    lint 7

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,40 +1,5 @@
 import NextAuth from "next-auth"
-import DiscordProvider from "next-auth/providers/discord"
-
-export const authOptions = {
-  providers: [
-    DiscordProvider({
-      clientId: process.env.DISCORD_CLIENT_ID!,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET!,
-      authorization: {
-        params: {
-          scope: "identify",
-        },
-      },
-    }),
-  ],
-  callbacks: {
-    async jwt({ token, account, profile }) {
-      // Persist the OAuth access_token and user info to the token right after signin
-      if (account) {
-        token.accessToken = account.access_token
-        token.id = profile?.id
-      }
-      return token
-    },
-    async session({ session, token }) {
-      // Send properties to the client
-      session.accessToken = token.accessToken as string
-      session.user.id = token.id as string
-      // Remove email from session
-      delete session.user.email
-      return session
-    },
-  },
-  pages: {
-    signIn: "/auth/signin",
-  },
-}
+import { authOptions } from "@/lib/auth"
 
 const handler = NextAuth(authOptions)
 
